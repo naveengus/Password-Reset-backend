dcdf1b02d46361317ec36fdac5327e4bb2973922
naveengus <naveen.g13579@gmail.com>
2024-10-16 00:19:14 +0530

    passwordreset

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import usersRoutes from "./users.js";
 const router = express.Router();
 
 router.get("/", (req, res) =>
-  res.send("<h1>project tracker</h1><p>well come, Its working Good</p>")
+  res.send("<h1>Reset Passwrd Backend</h1> <p>well come, Its working Good</p>")
 );
 router.use("/users", usersRoutes);
 export default router;
